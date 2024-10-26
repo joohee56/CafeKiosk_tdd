45aae72bb000bceb37cf648efd4409d636057b22
Joohui <joohuii96@gmail.com>
2024-10-26 20:28:43 +0900

    프로젝트 세팅

---
+++ src/main/java/sample/cafekiosk_tdd/CafekioskTddApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafekioskTddApplication.class, args);
+    }
+
+}
